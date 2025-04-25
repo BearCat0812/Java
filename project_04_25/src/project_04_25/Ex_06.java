a631deb76bce97cb1bd870e54ae3a89dab120eef
BearCat0812 <63222120+BearCat0812@users.noreply.github.com>
2025-04-25 16:40:32 +0900

    25.04.25

---
+++ project_04_25/src/project_04_25/Ex_06.java
@@ -0,0 +1,154 @@
+package project_04_25;
+
+import java.util.*;
+
+//class Person {
+//	String name;
+//	int age;
+//	
+//	void print() {
+//		System.out.println("이름: "+name+", 나이: "+age);
+//	}
+//}
+
+//class Car {
+//	String brand_name;
+//	int max_speed;
+//	
+//	void explain() {
+//		System.out.println(brand_name+"의 최고 스피드는 시속 "+max_speed+" km 입니다.");
+//	}
+//	
+//}
+
+//class Calc {
+//	int add(int a, int b) {
+//		return a + b;
+//	}
+//	int sub(int a, int b) {
+//		return a - b;
+//	}
+//	int mul(int a, int b) {
+//		return a * b;
+//	}
+//	int div(int a, int b) {
+//		return a / b;
+//	}
+//}
+
+//class Pet {
+//	String name;
+//	int age;
+//	String species;
+//	String sound;
+//	
+//	String Sound(String a) {
+//		if(a.equals("개") || a.equals("강아지")) {
+//			sound = "멍멍";
+//		}else if(a.equals("고양이")){
+//			sound = "야옹";
+//		}else if(a.equals("새")){
+//			sound = "짹짹";
+//		}else {
+//			sound = "등록되어 있지 않은 종입니다.";
+//		}
+//		return sound;
+//	}
+//	
+////	void bark() {
+////		System.out.println("멍멍");
+////	}
+////	void meow() {
+////		System.out.println("야옹");
+////	}
+////	void bird() {
+////		System.out.println("짹짹");
+////	}
+//
+//	
+//}
+
+public class Ex_06 {
+
+	public static void main(String[] args) {
+		
+//		Person p1 = new Person();
+//		Person p2 = new Person();
+//		
+//		p1.name = "철수";
+//		p1.age = 25;
+//		
+//		p2.name = "영희";
+//		p2.age = 23;
+//
+//		
+//		p1.print();
+//		p2.print();
+		
+//		Car c1 = new Car();
+//		Car c2 = new Car();
+//		
+//		c1.brand_name = "벤츠";
+//		c1.max_speed = 250;
+//		x
+//		c2.brand_name = "아우디";
+//		c2.max_speed = 230;
+//
+//		c1.explain();
+//		c2.explain();
+		
+//		int a = 0;
+//		int b = 0;
+//		String op = new String();
+//		
+//		Calc calc = new Calc();
+//
+//		Scanner sc = new Scanner(System.in);
+//		
+//		for(int i = 0 ; i < 2; i++) {
+//			System.out.print(i+1+"번째 숫자를 입력해주세요: ");
+//			if(i == 0) {
+//				a=sc.nextInt();
+//				System.out.print("연산자를 입력해주세요: ");
+//				op = sc.next();
+//			}else if(i == 1) {
+//				b=sc.nextInt();
+//			}
+//		}
+//		
+//		sc.close();
+//		
+//		if(op.equals("+")) {
+//			System.out.println(a+" "+op+" "+b+" = "+ calc.add(a, b));
+//		}else if(op.equals("-")) {
+//			System.out.println(a+" "+op+" "+b+" = "+ calc.sub(a, b));
+//		}else if(op.equals("*")) {
+//			System.out.println(a+" "+op+" "+b+" = "+ calc.mul(a, b));
+//		}else if(op.equals("/")) {
+//			System.out.println(a+" "+op+" "+b+" = "+ calc.div(a, b));
+//		}else {
+//			System.out.println("연산자를 제대로 입력해주세요");
+//		}
+				
+//		Pet pet = new Pet();
+//
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("이름을 입력해주세요.: ");
+//		
+//		pet.name = sc.next();
+//		
+//		System.out.print("나이를 입력해주세요.: ");
+//
+//		pet.age = sc.nextInt();
+//		
+//		System.out.print("종을 입력해주세요.: ");
+//		
+//		pet.species = sc.next();
+//		
+//		sc.close();
+//		
+//		System.out.println(pet.Sound(pet.species));
+	}
+
+}
