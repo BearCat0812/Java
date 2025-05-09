8a018c482a3772c68c2bf32fe8bb0d09734d5d68
BearCat0812 <63222120+BearCat0812@users.noreply.github.com>
2025-05-09 17:33:50 +0900

    25.05.09

---
+++ project_05_09/src/project_05_09/Ex_13.java
@@ -0,0 +1,17 @@
+package project_05_09;
+
+import java.util.Arrays;
+
+public class Ex_13 {
+
+	public static void main(String[] args) {
+		int[] arr = { 4, 7, 9, 5, 1 };
+		Arrays.sort(arr);
+//		arr.sort
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
